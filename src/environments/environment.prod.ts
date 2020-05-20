6fee5858cc2141fb92575213205665d1762d33b1
Unknown <rodrigofranco30@gmail.com>
2020-05-20 11:29:43 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  ms_produto_url:'https://tbnp-ms-produto.herokuapp.com/produto',
+  ms_movimento_url:'https://tbnp-cons-manutencao.herokuapp.com/movimento'
 };
